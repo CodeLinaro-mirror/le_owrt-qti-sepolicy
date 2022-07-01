dd35dd46df33b3bcbb67c6daabffea430e786314
Ravi Kumar Siddojigari <quic_rsiddoji@quicinc.com>
2022-07-01 21:19:06 +0530

    owrt-sepolicy: updated the permission based on bootup denials
    making sure the bootup denails are addressed .
    Based on the use case we might need to further update this.

---
+++ files/selinux-labeldev.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+ 
+/usr/sbin/selinuxenabled 2>/dev/null || exit 0
+
+CHCON=/usr/bin/chcon
+MATCHPATHCON=/usr/sbin/matchpathcon
+RESTORECON=/sbin/restorecon
+ 
+for i in ${CHCON} ${MATCHPATHCON} ${RESTORECON}; do
+	test -x $i && continue
+	echo "$i is missing in the system."
+	echo "Please add \"selinux=0\" in the kernel command line to disable SELinux."
+	exit 1
+done
+
+# Because /dev/console is not relabeled by kernel, many commands
+# would can not use it, including restorecon.
+${CHCON} -t `${MATCHPATHCON} -n /dev/null | cut -d: -f3` /dev/null
+${CHCON} -t `${MATCHPATHCON} -n /dev/console | cut -d: -f3` /dev/console
+
+# Now, we should relabel /dev for most services
+# only thing in tmpfs are to be done here other should move
+# from this file over the period of time
+${RESTORECON} -RF /dev
+${RESTORECON} -RF /etc
+#TODO : this 2 line need to removed
+#       to later stage
+${RESTORECON} -RF /usr  /bin  /sbin
+exit 0
